127461d5cb0b17ec8570a8816b5ee0307776e99e
Gleb Bychkov <bychkov-bgo@yandex.ru>
2022-05-06 15:16:27 +0300

    Updated open/closePopup, started created insertion function

--- scripts/index.js
+++ scripts/index.js
@@ -21,37 +21,51 @@ const popupImagePic = popupImage.querySelector('.popup__image');
 const popupImageTitle = popupImage.querySelector('.popup__title-image');
 const buttonCloseImage = popupImage.querySelector('.popup__close-button_image');
 
-function openPopup() {
+const elementTemplate = document.querySelector('#default-element').content.querySelector('.element');
+
+function openPopup(popup) {
   nameInput.value = infoName.textContent;
   activityInput.value = infoActivity.textContent;
-  popupElement.classList.add('popup_opened');
+  placeInput.value = "";
+  linkInput.value = "";
+  popup.classList.add('popup_opened');
 }
 
-function closePopup() {
-  popupElement.classList.remove('popup_opened');
-  popupAddCard.classList.remove('popup_opened');
-  popupImage.classList.remove('popup_opened');
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
 }
+
+// function openPopup() {
+//   nameInput.value = infoName.textContent;
+//   activityInput.value = infoActivity.textContent;
+//   popupElement.classList.add('popup_opened');
+// }
+
+// function closePopup() {
+//   popupElement.classList.remove('popup_opened');
+//   popupAddCard.classList.remove('popup_opened');
+//   popupImage.classList.remove('popup_opened');
+// }
 
 function formSubmitHandler(evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы
   infoName.textContent = nameInput.value;
   infoActivity.textContent = activityInput.value;
-  closePopup();
+  closePopup(popupElement);
 }
 
 function addDefaultElement(imageLink, placeName) {
-  const elementTemplate = document.querySelector('#default-element').content;
-  const currentElement = elementTemplate.querySelector('.element').cloneNode(true);
-
-  currentElement.querySelector('.element__image').src = imageLink;
-  currentElement.querySelector('.element__image').alt = placeName;
-  currentElement.querySelector('.element__image').addEventListener('click', (evt) => {
+  const currentElement = elementTemplate.cloneNode(true);
+  const elementImage = currentElement.querySelector('.element__image');
+  elementImage.src = imageLink;
+  elementImage.alt = placeName;
+  elementImage.addEventListener('click', (evt) => {
     popupImagePic.src = imageLink;
     popupImagePic.alt = placeName;
     popupImageTitle.textContent = placeName;
-    openPopupImage();
-  })
+    // openPopupImage();
+    openPopup(popupImage);
+  });
   currentElement.querySelector('.element__place').textContent = placeName;
   currentElement.querySelector('.element__like').addEventListener('click', function(evt) {
     evt.target.classList.toggle('element__like_active');
@@ -60,14 +74,21 @@ function addDefaultElement(imageLink, placeName) {
     currentElement.remove();
   });
 
-  sectionElements.prepend(currentElement);
+  // return currentElement; /////////////
+  console.log(currentElement);
+  sectionElements.prepend(currentElement); //////////////////
 }
 
-function openPopupAddCard() {
-  placeInput.value = "";
-  linkInput.value = "";
-  popupAddCard.classList.add('popup_opened');
-}
+function elementPaste(element) { /////////////
+  sectionElements.prepend(element); //////////////
+} ///////////
+
+
+// function openPopupAddCard() {
+//   placeInput.value = "";
+//   linkInput.value = "";
+//   popupAddCard.classList.add('popup_opened');
+// }
 
 // function onDocumentKeyUp(event) {
 //   if (event.keyCode === 107) {
@@ -80,16 +101,17 @@ function formCreateHandler(evt) {
   const currentPlaceInput = placeInput.value;
   const currentLinkInput = linkInput.value;
   addDefaultElement(currentLinkInput, currentPlaceInput);
-  closePopup();
+  // elementPaste(currentElement); //////////////////
+  closePopup(popupAddCard);
 }
 
-function openPopupImage () {
-  popupImage.classList.add('popup_opened');
-}
+// function openPopupImage () {
+//   popupImage.classList.add('popup_opened');
+// }
 
+buttonEdit.addEventListener('click', () => openPopup(popupElement));
 
-buttonEdit.addEventListener('click', openPopup);
-buttonClose.addEventListener('click', closePopup);
+buttonClose.addEventListener('click', () => closePopup(popupElement));
 
 formElement.addEventListener('submit', formSubmitHandler);
 
@@ -97,15 +119,16 @@ initialCards.forEach(function (item) {
   const imageLink = item.link;
   const placeName = item.name;
   addDefaultElement(imageLink, placeName);
+  // elementPaste(currentElement); //////////////
 });
 
-buttonAdd.addEventListener('click', openPopupAddCard);
-buttonCloseAddCard.addEventListener('click', closePopup);
+buttonAdd.addEventListener('click', () => openPopup(popupAddCard));
+buttonCloseAddCard.addEventListener('click', () => closePopup(popupAddCard));
 // document.addEventListener('keyup', onDocumentKeyUp);
 
 formAddCardElement.addEventListener('submit', formCreateHandler);
 
-buttonCloseImage.addEventListener('click', closePopup);
+buttonCloseImage.addEventListener('click', () => closePopup(popupImage));
 
 
 
