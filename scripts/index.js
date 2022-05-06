fb8ff329f27b6bfabac7dda7e8e468e01502d6da
Gleb Bychkov <bychkov-bgo@yandex.ru>
2022-05-07 01:59:44 +0300

    changed openPopup function -> callback function of edit and add buttons. Changed name of some functions

--- scripts/index.js
+++ scripts/index.js
@@ -6,28 +6,20 @@ const nameInput = formProfile.querySelector('.popup__input_data_name');
 const activityInput = formProfile.querySelector('.popup__input_data_activity');
 const infoName = document.querySelector('.profile__name');
 const infoActivity = document.querySelector('.profile__activity');
-//------------------------------------------------------------------
 const popupAddCard = document.querySelector('.popup_addcard');
 const buttonAdd = document.querySelector('.profile__add-button');
 const buttonCloseAddCard = popupAddCard.querySelector('.popup__close-button_addcard');
 const formAddCardElement = document.querySelector('.popup__form_addcard');
 const placeInput = formAddCardElement.querySelector('.popup__input_data_place');
 const linkInput = formAddCardElement.querySelector('.popup__input_data_link');
-
 const sectionElements = document.querySelector('.elements');
-
 const popupImage = document.querySelector('.popup_image');
 const popupImagePic = popupImage.querySelector('.popup__image');
 const popupImageTitle = popupImage.querySelector('.popup__title-image');
 const buttonCloseImage = popupImage.querySelector('.popup__close-button_image');
-
 const elementTemplate = document.querySelector('#default-element').content.querySelector('.element');
 
 function openPopup(popup) {
-  nameInput.value = infoName.textContent;
-  activityInput.value = infoActivity.textContent;
-  placeInput.value = "";
-  linkInput.value = "";
   popup.classList.add('popup_opened');
 }
 
@@ -35,8 +27,8 @@ function closePopup(popup) {
   popup.classList.remove('popup_opened');
 }
 
-function formSubmitHandler(evt) {
-  evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы
+function SubmitHandler(evt) {
+  evt.preventDefault();
   infoName.textContent = nameInput.value;
   infoActivity.textContent = activityInput.value;
   closePopup(popupProfile);
@@ -60,39 +52,47 @@ function addDefaultElement(imageLink, placeName) {
   currentElement.querySelector('.element__delete-button').addEventListener('click', () => {
     currentElement.remove();
   });
-  // sectionElements.prepend(currentElement); 
   return currentElement;
 }
 
-function elementPaste(element) {
+function pasteElement(element) {
   sectionElements.prepend(element);
 }
 
-function formCreateHandler(evt) {
+function CreateHandler(evt) {
   evt.preventDefault();
   const currentPlaceInput = placeInput.value;
   const currentLinkInput = linkInput.value;
   const insertElement = addDefaultElement(currentLinkInput, currentPlaceInput);
-  elementPaste(insertElement);
+  pasteElement(insertElement);
   closePopup(popupAddCard);
 }
 
-buttonEdit.addEventListener('click', () => openPopup(popupProfile));
+buttonEdit.addEventListener('click', () => {
+  nameInput.value = infoName.textContent;
+  activityInput.value = infoActivity.textContent;
+  openPopup(popupProfile);
+});
 
 buttonCloseProfile.addEventListener('click', () => closePopup(popupProfile));
 
-formProfile.addEventListener('submit', formSubmitHandler);
+formProfile.addEventListener('submit', SubmitHandler);
 
 initialCards.forEach(function (item) {
   const imageLink = item.link;
   const placeName = item.name;
-  elementPaste(addDefaultElement(imageLink, placeName));
+  pasteElement(addDefaultElement(imageLink, placeName));
 });
 
-buttonAdd.addEventListener('click', () => openPopup(popupAddCard));
+buttonAdd.addEventListener('click', () => {
+  placeInput.value = "";
+  linkInput.value = "";
+  openPopup(popupAddCard);
+});
+
 buttonCloseAddCard.addEventListener('click', () => closePopup(popupAddCard));
 
-formAddCardElement.addEventListener('submit', formCreateHandler);
+formAddCardElement.addEventListener('submit', CreateHandler);
 
 buttonCloseImage.addEventListener('click', () => closePopup(popupImage));
 
